7ca9ad0f91de8cb35a0a4a95e6387b46af1b2888
geekan <geekan@foxmail.com>
2015-10-21 23:30:41 +0800

    follow pypi tutorial

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name = 'psu',
+    version = '0.0.1',
+    keywords = ('shell', 'utils', 'os'),
+    description = 'Python shell-like utilities.',
+    license = 'MIT License',
+
+    author = 'alexapollo0',
+    author_email = 'alexapollo0@gmail.com',
+
+    # packages = find_packages(),
+    packages=['psu',],
+    platforms = 'any',
+)
--- test.py
+++
@@ -1,7 +0,0 @@
-from psu import mkdir, ls, cd, touch
-
-path = './test'
-print(mkdir(path))
-print(cd(path))
-print(touch('x'))
-print(ls('.'))
